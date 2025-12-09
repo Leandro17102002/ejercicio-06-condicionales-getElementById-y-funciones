d607cc676367ab257b7d32dcacfe792f5b809cb9
Leandro <leandrogpelozo1@gmail.com>
2025-12-08 22:40:47 -0300

    Se optimizan procesos a traves de funciones

--- js/main.js
+++ js/main.js
@@ -14,66 +14,58 @@ let nombresApellidos2 = [
     document.getElementById('primerApellido2'),
     document.getElementById('segundoApellido2')];
 
-let nombreCompleto1 = '';
-let nombreCompleto2 = '';
 
-for (let i = 0; i < nombresApellidos1.length; i++) {
-    if (nombresApellidos1[i].textContent !== '') {
-        let texto1 = nombresApellidos1[i].textContent;
-        if (i>=2){
-            texto1 = texto1.toUpperCase();
-        }
-        nombreCompleto1 += texto1 + ' ';
+function construirNombreCompleto(lista) {
+    let textoFinal = '';
+    for (let i=0; i < lista.length; i++){
+        let texto = lista[i].textContent;
+
+        if (texto !== ''){
+            if (i>=2){
+                texto = texto.toUpperCase();
+            }
+            textoFinal += texto + ' ';
+        }   
     }
-    if (nombresApellidos2[i].textContent !== '') {
-        texto2 = nombresApellidos2[i].textContent;
-        if (i>=2){
-            texto2 = texto2.toUpperCase();
+    return textoFinal;
+}
+
+function buscarCoincidencias(lista1, lista2, inicio, fin, tipo){
+    for (let i = inicio; i <= fin; i++) {
+        let item1 = lista1[i].textContent.toLowerCase();
+
+        for (let j = inicio; j <= fin; j++) {
+            let item2 = lista2[j].textContent.toLowerCase();
+            
+            if (item1 != '' && item1 === item2) {
+                console.log(`Se encontro una coincidencia de ${tipo}: "${item1}"`);
+                color = prompt(
+                    `Se encontró una coincidencia de ${tipo}: "${item1}".\n` +
+                    `¿Con qué color desea resaltarlo?\n` +
+                    `Ej: red, blue, green, #FF0000, #00FF00, etc.`);
+
+                lista1[i].style.color = `${color}`;
+                lista2[j].style.color = `${color}`;
+            } else{
+                console.log(`No hay coincidencias entre ${tipo}`);
+            }
         }
-        nombreCompleto2 += texto2 + ' ';
     }
 }
+
+let nombreCompleto1 = construirNombreCompleto(nombresApellidos1);
+let nombreCompleto2 = construirNombreCompleto(nombresApellidos2);
+
 
 console.log(`-----
 integrante 1: "${nombreCompleto1}"
 integrante 2: "${nombreCompleto2}"
 ----- `);
 
-for (let i = 0; i < 2; i++) { 
-    let n1 = nombresApellidos1[i].textContent.toLowerCase();
+buscarCoincidencias(nombresApellidos1, nombresApellidos2, 0, 1, 'nombres');
 
-    for (let j = 0; j < 2; j++) { 
-        let n2 = nombresApellidos2[j].textContent.toLowerCase();
+let confirmacionApellidos = confirm('Desea buscar coincidencias entre apellidos?');
 
-        if (n1 !== '' && n1 === n2) {
-            console.log(`Se encontro una coincidencia de nombres: "${n1}"`);
-            color = prompt(`Se encontro una coincidencia de nombres: "${n1}".
-                Con que color desea resaltarlo? (ingrese el nombre del color en ingles, por ejemplo: red, blue, green, etc., o bien su codigo hexadecimal, por ejemplo: #FF0000, #00FF00, etc.)`);
-            nombresApellidos1[i].style.color = `${color}`;
-            nombresApellidos2[j].style.color = `${color}`;
-        } else {
-            console.log('No hay coincidencias entre nombres.');
-        }
-    }
-}
-
-confirmacion = confirm('Desea buscar coincidencias entre apellidos?');
-
-if (confirmacion) {
-    for (let i = 2; i < 4; i++) { 
-        let a1 = nombresApellidos1[i].textContent.toLowerCase();
-
-        for (let j = 2; j < 4; j++) { 
-            let a2 = nombresApellidos2[j].textContent.toLowerCase();
-            if (a1 !== '' && a1 === a2) {
-                console.log(`Se encontro una coincidencia de apellidos: "${a1}"`);
-                color = prompt(`Se encontro una coincidencia de apellidos: "${a1}".
-                    Con que color desea resaltarlo? (ingrese el nombre del color en ingles, por ejemplo: red, blue, green, etc., o bien su codigo hexadecimal, por ejemplo: #FF0000, #00FF00, etc.)`);
-                nombresApellidos1[i].style.color = `${color}`;
-                nombresApellidos2[j].style.color = `${color}`;
-            } else {
-                console.log('No hay coincidencias entre apellidos.');
-            }
-        }
-    }
+if (confirmacionApellidos) {
+    buscarCoincidencias(nombresApellidos1, nombresApellidos2, 2, 3, 'apellidos')
 }

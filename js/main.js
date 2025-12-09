5c050308e40fe96081b6e4863c0d5146a60b36ae
Leandro <leandrogpelozo1@gmail.com>
2025-12-08 22:12:04 -0300

    Se agregan controles de coincidencia entre nombres y apellidos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
-/* Punto 1 */
 let title = document.getElementById('title');
 console.log(title.textContent);
 
 
-/* Punto 3 */
 let nombresApellidos1 = [
     document.getElementById('primerNombre1'), 
     document.getElementById('segundoNombre1'),
@@ -41,7 +39,41 @@ integrante 1: "${nombreCompleto1}"
 integrante 2: "${nombreCompleto2}"
 ----- `);
 
+for (let i = 0; i < 2; i++) { 
+    let n1 = nombresApellidos1[i].textContent.toLowerCase();
 
-/* Punto 4 */
+    for (let j = 0; j < 2; j++) { 
+        let n2 = nombresApellidos2[j].textContent.toLowerCase();
 
+        if (n1 !== '' && n1 === n2) {
+            console.log(`Se encontro una coincidencia de nombres: "${n1}"`);
+            color = prompt(`Se encontro una coincidencia de nombres: "${n1}".
+                Con que color desea resaltarlo? (ingrese el nombre del color en ingles, por ejemplo: red, blue, green, etc., o bien su codigo hexadecimal, por ejemplo: #FF0000, #00FF00, etc.)`);
+            nombresApellidos1[i].style.color = `${color}`;
+            nombresApellidos2[j].style.color = `${color}`;
+        } else {
+            console.log('No hay coincidencias entre nombres.');
+        }
+    }
+}
 
+confirmacion = confirm('Desea buscar coincidencias entre apellidos?');
+
+if (confirmacion) {
+    for (let i = 2; i < 4; i++) { 
+        let a1 = nombresApellidos1[i].textContent.toLowerCase();
+
+        for (let j = 2; j < 4; j++) { 
+            let a2 = nombresApellidos2[j].textContent.toLowerCase();
+            if (a1 !== '' && a1 === a2) {
+                console.log(`Se encontro una coincidencia de apellidos: "${a1}"`);
+                color = prompt(`Se encontro una coincidencia de apellidos: "${a1}".
+                    Con que color desea resaltarlo? (ingrese el nombre del color en ingles, por ejemplo: red, blue, green, etc., o bien su codigo hexadecimal, por ejemplo: #FF0000, #00FF00, etc.)`);
+                nombresApellidos1[i].style.color = `${color}`;
+                nombresApellidos2[j].style.color = `${color}`;
+            } else {
+                console.log('No hay coincidencias entre apellidos.');
+            }
+        }
+    }
+}
